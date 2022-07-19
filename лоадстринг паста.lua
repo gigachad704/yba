f588725b87cc6525bd71d4fce8d0ace3551874a5
gigachad704 <109565668+gigachad704@users.noreply.github.com>
2022-07-19 15:16:27 +1200

    Add files via upload

---
+++ лоадстринг паста.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gigachad704/yba/main/%D0%A1%D0%B1%D0%BE%D1%80%20%D0%BF%D0%B0%D1%81%D1%82%D0%B0.lua?token=GHSAT0AAAAAABWXZOUBFLY5HH4KLNF6JUE2YWWD5QQ"))()
